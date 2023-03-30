0802a6b06bc5a9c5a6931cb0a574fa0d2dc82feb
Manuel_Desole <manuel.desole@carol.health>
2023-03-30 03:12:43 +0200

    [New]: fizzbuzz returns Fizz for 3

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -5,5 +5,7 @@ describe("fizzbuzz", () => {
     it('returns a string', () => {
         expect(typeof fizzBuzz(5)).toBe('string')
     })
-
+    it('returns Fizz for multiples of three', () => {
+        expect(fizzBuzz(3)).toEqual('Fizz')
+    })
 });
